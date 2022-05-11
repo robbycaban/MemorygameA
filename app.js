310f11a523e5553de31decf3778886449fb67688
Naomi Brown <iamnaomibrown@gmail.com>
2022-05-11 10:54:57 -0400

    test

---
+++ app.js
@@ -0,0 +1,18 @@
+import'./App.css'; 
+import MemoryCard from "./Components/MemoryCard"
+
+function App() {
+    return (
+        <div className='App'>
+            <header className="App-header">
+                <header className="App=header"> 
+                <h1>Memory Game</h1>
+                <h4>Match Cards to Win</h4>
+        <div>
+        <MemoryCard /> 
+        <MemoryCard /> 
+        <MemoryCard /> 
+        </div> 
+        <div> 
+    )
+}
