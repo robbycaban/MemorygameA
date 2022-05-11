ae88d1e9829c8a63a5c01362273d5bfd0645f8fd
Naomi Brown <iamnaomibrown@gmail.com>
2022-05-11 11:22:58 -0400

    testing second file

---
+++ src/App.js
@@ -0,0 +1,46 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default App;
+
+import'./App.css'; 
+import MemoryCard from "./Components/MemoryCard"
+
+function App() {
+    return (
+        <div className='App'>
+            <header className="App-header">
+                <header className="App=header"> 
+                <h1>Memory Game</h1>
+                <h4>Match Cards to Win</h4>
+
+                <h1> this message is for a test </h1>
+        <div>
+        <MemoryCard />  
+        <MemoryCard /> test to test GH
+        <MemoryCard /> 
+        {/* </div> robby is about to save in app.js 
+        <div>  */}
+    )
+}
